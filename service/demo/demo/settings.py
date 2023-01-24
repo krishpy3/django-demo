4f90b90bffdcc895ae36844f3e55b3106f35d04c
Krish <krishpy3@gmail.com>
2023-01-25 00:42:46 +0530

    test

--- service/demo/demo/settings.py
+++ service/demo/demo/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-gsugnh$4dhk^57s*rsw1_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
