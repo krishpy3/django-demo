7ff391403cf664b087d3f67c78fef4c7ef52323f
Krish <krishpy3@gmail.com>
2023-01-24 22:45:12 +0530

    test

--- service/demo/demo/settings.py
+++ service/demo/demo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gsugnh$4dhk^57s*rsw1_3^ye99dlj(8k&@svt1vj-vcbt1^7&'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-gsugnh$4dhk^57s*rsw1_3^ye99dlj(8k&@svt1vj-vcbt1^7&')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
